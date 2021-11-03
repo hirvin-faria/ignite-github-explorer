1938a193cc10320a29a5abf86e382032a70f39bc
hirvin-faria <hirvin.nogueira.faria@gmail.com>
2021-11-02 21:56:49 -0300

    feat: adicionando configuracao do webpack

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src', 'index.jsx'),                         // arquivo de entrada da aplicação
+    output: {
+        path: path.resolve(__dirname, 'dist'),                                  // pasta onde sera gerada o bundle
+        filename: 'bundle.js'                                                   // nome do arquivo do bundle
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],                                            // extenções que o webpack deve resolver dentro da aplicação
+    },
+    module: {                                                                   // configuração/regras para resolução de arquivos do webpack
+        rules: [
+            {
+                test: /\.jsx$/,                                                 // expressao regular para identificar o arquivo
+                exclude: /node_modules/,                                        // expressao regular de arquivos a ser excluidos
+                use: 'babel-loader'                                             // plugin do babel para integração do webpack
+            },
+        ],
+    }
+};
