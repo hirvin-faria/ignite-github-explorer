e785a913b9c37130aa11748b26491e4916d694ec
hirvin-faria <hirvin.nogueira.faria@gmail.com>
2021-11-02 22:11:10 -0300

    feat: configurando o react no index.html

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,8 @@
 module.exports = {
     presets: [
         '@babel/preset-env',
-        '@babel/preset-react'
+        ['@babel/preset-react', { 
+            runtime: 'automatic'                                                // configura para que não seja nescessario importar explicitamente o react
+        }]
     ]
 };
